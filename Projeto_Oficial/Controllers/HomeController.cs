2a6e5b6363edfa6ced45576a79425678f9a9cfcc
Fabricio Santos <fabriciosan47@gmail.com>
2020-09-28 17:09:17 -0400

    Adicionar arquivos de projeto.

---
+++ Projeto_Oficial/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NToastNotify;
+using Projeto_Oficial.Models;
+
+namespace Projeto_Oficial.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IToastNotification _toastNotification;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IToastNotification toastNotification)
+        {
+            _logger = logger;
+            _toastNotification = toastNotification;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Projeto_Oficial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_Oficial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
